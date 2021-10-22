c672f90c38952d499f52d2365810654d4c94f74b
Евгений Евгений <berezin.evgenij.01@mail.ru>
2021-10-22 12:46:05 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+
+namespace WpfApp1
+{
+	class MainViewModel : INotifyPropertyChanged
+	{
+		public ICommand MyCommand { get; }
+
+		private string _expression;
+
+		public string Expression
+		{
+			get => _expression;
+			set
+			{
+				_expression = value;
+				OnPropertyChanged(nameof(Expression));
+			}
+		}
+
+		public MainViewModel()
+		{
+			MyCommand = new RelayCommand<string>(s => Expression+="123");
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+	}
+}
---
+++ WpfApp1/Parser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+	class Parser
+	{
+		public string OutputString { get; set; }
+
+		public string ParseExpression(string inputString)
+		{
+			inputString.Remove(0, 1);
+
+			return OutputString;
+		}
+
+	}
+}
